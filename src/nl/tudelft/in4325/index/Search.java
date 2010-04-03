031b0edc755262187314021764793f3ca4df074e
zandmans <zandmans@d204f34c-dc34-e9b8-d593-7c8555d54f4f>
2010-04-03 13:50:04 +0000

    Finished the boolean search so far, added comments to the code and made some changes to the existing code like replacing the Vector with an ArrayList. Right now I added myself as author to several functions I wrote, I did this so you know who to talk to for an explanation or to make changes. We can delete all those @author comments in the end.

--- src/nl/tudelft/in4325/index/Search.java
+++ src/nl/tudelft/in4325/index/Search.java
@@ -1,66 +1,291 @@
+/**
+ * This class contains all the different search methods
+ * 
+ * @version 0.1
+ */
+
 package nl.tudelft.in4325.index;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
-import java.util.Vector;
 
 import nl.tudelft.in4325.main.Main;
 
 public class Search {
 
-	private static Vector<Vector<Integer>> results;
+	/* variable declarations */
+	private static ArrayList<ArrayList<Integer>> results;
 	
+	/**
+	 * Performs a boolean search on the given query
+	 * 
+	 * @author Thijs Zandvliet
+	 * @param query
+	 */
 	public static void booleanSearch(String query)
 	{
+		/* create a new scanner which reads the query one word at the time */
 		Scanner sc = new Scanner(query);
-		results = new Vector<Vector<Integer>>();
 		
+		/* create a new ArrayList for the results */
+		results = new ArrayList<ArrayList<Integer>>();
+		
+		/* as long as the scanner reads new words... */
 		while(sc.hasNext())
 		{
-			String keyword = sc.next();
+			/* normalize the next word and save it into the string keyword */
+			String keyword = Main.formatString(sc.next());
+			
+			/* if the ArrayList results already contains elements... */
 			if(results.size() > 0)
 			{
-				if(sc.hasNext())
+				/* if one of the boolean terms is used and there's a next word to read... */
+				if((keyword.equals("or") || keyword.equals("and") || keyword.equals("not")) && sc.hasNext())
 				{
-					Vector<Integer> result = Main.index.get(sc.next());
+					/* search for the next word in the Hashtable and add it to the results */
+					ArrayList<Integer> result = Main.index.get(sc.next());
 					results.add(result);
 				}
 				else
 				{
-					System.err.println("Syntax error in Query");
-					return;
+					/* search for the keyword in the Hashtable and add it to the results */
+					ArrayList<Integer> result = Main.index.get(keyword);
+					results.add(result);
 				}
-				if(keyword.equals("OR")) booleanOR();
-				else if(keyword.equals("AND")) booleanAND();
-				else if(keyword.equals("NOT")) booleanNOT();
-				else booleanOR();
+				
+				/* receive the first two ArrayLists */
+				ArrayList<Integer> r1 = results.get(0);
+				ArrayList<Integer> r2 = results.get(1);
+				
+				/* find the right function for the right term */
+				if(keyword.equals("or")) booleanOR(r1, r2);
+				else if(keyword.equals("and")) booleanAND(r1, r2);
+				else if(keyword.equals("not")) booleanNOT(r1, r2);
+				else booleanOR(r1, r2);
 			}
+			
+			/* if the ArrayList results is empty, search for the keyword in the Hashtable and add it to the results */
 			else
 			{
-				Vector<Integer> result = Main.index.get(keyword);
+				ArrayList<Integer> result = Main.index.get(keyword);
 				results.add(result);
 			}
 		}
-	}
-	
-	
-	public static void booleanOR()
-	{
-		System.out.println("OR-query");
+		
+		/* print the results */
 		System.out.println(results);
 	}
 	
 	
-	public static void booleanAND()
+	/**
+	 * Boolean OR-function
+	 * Compares the first two ArrayLists of the results ArrayList, empties the results ArrayList
+	 * and if the answer isn't null, set's the new ArrayList as first record of results
+	 * 
+	 * @author Thijs Zandvliet
+	 */
+	public static void booleanOR(ArrayList<Integer> r1, ArrayList<Integer> r2)
 	{
-		System.out.println("AND-query");
-		System.out.println(results);
+		if(detectNull(r1, r2)) return;
+		
+		/* add a null-value at the end of both ArrayLists for the iterator */
+		r1.add(null);
+		r2.add(null);
+		
+		/* create two iterators for both ArrayLists */
+		Iterator<Integer> p1 = r1.iterator();
+		Iterator<Integer> p2 = r2.iterator();
+		
+		/* set the two first Integers so they can be compared */
+		Integer i1 = p1.next();
+		Integer i2 = p2.next();
+		
+		/* create a new ArrayList for the answer */
+		ArrayList<Integer> answer = new ArrayList<Integer>();
+		
+		/* as long as there are at least the null-values add the end, loop */
+		while(p1.hasNext() && p2.hasNext())
+		{
+			/* if both Integers are equal, add the answer and increase both iterators */
+			if(i1 == i2)
+			{
+				answer.add(i1);
+				i1 = p1.next();
+				i2 = p2.next();
+			}
+			
+			/* if the first Integer is smaller than the second, add it to the answer and increase the first iterator */
+			else if(i1 < i2)
+			{
+				answer.add(i1);
+				i1 = p1.next();
+			}
+			
+			/* else, add it to the answer and increase the second iterator */
+			else
+			{
+				answer.add(i2);
+				i2 = p2.next();
+			}
+		}
+		
+		/* clear the results ArrayList and add the answer iff not null */
+		results.clear();
+		if(answer != null)
+			results.add(answer);
 	}
 	
 	
-	public static void booleanNOT()
+	/**
+	 * Boolean AND-function
+	 * Compares the first two ArrayLists of the results ArrayList, empties the results ArrayList
+	 * and if the answer isn't null, set's the new ArrayList as first record of results
+	 * 
+	 * @author Thijs Zandvliet
+	 */
+	public static void booleanAND(ArrayList<Integer> r1, ArrayList<Integer> r2)
 	{
-		System.out.println("NOT-query");
-		System.out.println(results);
+		/* if one of the ArrayLists is empty, clear the results ArrayList and return */
+		if(r1 == null || r2 == null)
+		{
+			results.clear();
+			return;
+		}
+		
+		/* add a null-value at the end of both ArrayLists for the iterator */
+		r1.add(null);
+		r2.add(null);
+		
+		/* create a new ArrayList for the answer */
+		ArrayList<Integer> answer = new ArrayList<Integer>();
+		
+		/* create two iterators for both ArrayLists */
+		Iterator<Integer> p1 = r1.iterator();
+		Iterator<Integer> p2 = r2.iterator();
+		
+		/* set the two first Integers so they can be compared */
+		Integer i1 = p1.next();
+		Integer i2 = p2.next();
+		
+		/* as long as there are at least the null-values add the end, loop */
+		while(p1.hasNext() && p2.hasNext())
+		{
+			/* if both Integers are equal, add the answer and increase both iterators */
+			if(i1 == i2)
+			{
+				answer.add(i1);
+				i1 = p1.next();
+				i2 = p2.next();
+			}
+			
+			/* if the first Integer is smaller than the second, increase the first iterator */
+			else if(i1 < i2)
+				i1 = p1.next();
+			
+			/* else, increase the second iterator */
+			else
+				i2 = p2.next();
+		}
+		
+		/* clear the results ArrayList and add the answer iff not null */
+		results.clear();
+		if(answer != null)
+			results.add(answer);
+	}
+	
+	
+	/**
+	 * Boolean NOT-function
+	 * Compares the first two ArrayLists of the results ArrayList, empties the results ArrayList
+	 * and if the answer isn't null, set's the new ArrayList as first record of results
+	 * 
+	 * @author Thijs Zandvliet
+	 */
+	public static void booleanNOT(ArrayList<Integer> r1, ArrayList<Integer> r2)
+	{
+		if(detectNull(r1, r2)) return;
+		
+		/* add a null-value at the end of both ArrayLists for the iterator */
+		r1.add(null);
+		r2.add(null);
+		
+		/* create a new ArrayList for the answer */
+		ArrayList<Integer> answer = new ArrayList<Integer>();
+		
+		/* create two iterators for both ArrayLists */
+		Iterator<Integer> p1 = r1.iterator();
+		Iterator<Integer> p2 = r2.iterator();
+		
+		/* set the two first Integers so they can be compared */
+		Integer i1 = p1.next();
+		Integer i2 = p2.next();
+		
+		/* as long as there are at least the null-values add the end, loop */
+		while(p1.hasNext() && p2.hasNext())
+		{
+			/* if both Integers are equal, increase both iterators */
+			if(i1 == i2)
+			{
+				i1 = p1.next();
+				i2 = p2.next();
+			}
+			
+			/* if the first Integer is smaller than the second, increase the first iterator */
+			else if(i1 < i2)
+			{
+				answer.add(i1);
+				i1 = p1.next();
+			}
+			
+			/* else, increase the second iterator */
+			else
+			{
+				answer.add(i1);
+				i2 = p2.next();
+			}
+		}
+		
+		/* clear the results ArrayList and add the answer iff not null */
+		results.clear();
+		if(answer != null)
+			results.add(answer);
+	}
+	
+	
+	/**
+	 * Detect if one the ArrayLists is empty
+	 * 
+	 * @author Thijs Zandvliet
+	 * @param r1
+	 * @param r2
+	 * @return boolean
+	 */
+	public static boolean detectNull(ArrayList<Integer> r1, ArrayList<Integer> r2)
+	{
+		/* if one of the ArrayLists is empty, clear the results */
+		if(r1 == null || r2 == null)
+			results.clear();
+		
+		/* if both ArrayLists are empty, return true */
+		if(r1 == null && r2 == null)
+			return true;
+		
+		/* if r1 is empty, add r2 to the results and return true */
+		else if(r1 == null)
+		{
+			results.add(r2);
+			return true;
+		}
+		
+		/* if r2 is empty, add r1 to the results and return true */
+		else if(r2 == null)
+		{
+			results.add(r1);
+			return true;
+		}
+		
+		return false;
 	}
 	
 }
--- src/nl/tudelft/in4325/main/Main.java
+++ src/nl/tudelft/in4325/main/Main.java
@@ -1,11 +1,17 @@
+/**
+ * This class contains the main functions to make the search-engine work
+ * 
+ * @version 0.1
+ */
+
 package nl.tudelft.in4325.main;
 
 import java.io.FileNotFoundException;
 import java.text.Normalizer;
+import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.Hashtable;
 import java.util.Scanner;
-import java.util.Vector;
 
 import nl.tudelft.in4325.index.Indexer;
 import nl.tudelft.in4325.index.Search;
@@ -13,45 +19,83 @@ import nl.tudelft.in4325.index.Search;
 public class Main
 {
 
-	public static Hashtable<String, Vector<Integer>> index;
+	/* variable declarations */
+	public static Hashtable<String, ArrayList<Integer>> index;
 	
+	
+	/**
+	 * Java main function
+	 * Makes it possible to input search queries and to perform a specific search method on it
+	 * 
+	 * @param args
+	 * @throws FileNotFoundException
+	 */
 	public static void main(String[] args) throws FileNotFoundException
 	{
+		/* create a new scanner for the input from the console */
 		Scanner sc = new Scanner(System.in);
+		
+		/* create a new Indexer object */
 		Indexer indexer = new Indexer();
 		
+		/* create a new index */
 		indexer.init();
 		index = indexer.getIndex();
 		
+		/* notify the user about the number of words indexed */
 		System.out.println("Index build successfully, number of words indexed: " + index.size());
 		
+		/* create an 'infinite' loop for the input, break it when the input is q */
 		while(true)
 		{
+			/* create the possibility to input a query */
 			System.out.println("Query: ");
-			
 			String query = sc.nextLine();
+			
+			/* if the query is 'q', exit the program */
 			if(query.equals("q")) System.exit(0);
 			
+			/* perform a boolean search on the query */
 			Search.booleanSearch(query);
 		}
 	}
 	
 	
+	/**
+	 * Normalize the given string, 
+	 * the new format is lowercase and contains only alphabetical letters and numbers
+	 * 
+	 * @author Thijs Zandvliet
+	 * @param str
+	 * @return string
+	 */
 	public static String formatString(String str)
 	{
+		/* try to normalize the string */
 		try
 		{
+			/* normalize the string, make it lowercase and remove all special characters */
 			return Normalizer.normalize(str, Normalizer.Form.NFC)
 			.toLowerCase()
 			.replaceAll("[^a-zA-Z 0-9]+","");
 		}
+		
+		/* if the function fails,  notify the user and return null */
 		catch(Exception ex)
 		{
+			/* print the error and return null */
+			System.err.println(ex);
 			return null;
 		}
     }
 	
 	
+	/**
+	 * Testfunction
+	 * Print all the elements contained in the index
+	 * 
+	 * @author Thijs Zandvliet
+	 */
 	public static void testIndex()
 	{
 		Enumeration<String> e = index.keys();
