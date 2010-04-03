6e414b6b603cb7a78971cf895483d6443de5a907
zandmans <zandmans@d204f34c-dc34-e9b8-d593-7c8555d54f4f>
2010-04-03 13:50:04 +0000

    Finished the boolean search so far, added comments to the code and made some changes to the existing code like replacing the Vector with an ArrayList. Right now I added myself as author to several functions I wrote, I did this so you know who to talk to for an explanation or to make changes. We can delete all those @author comments in the end.

--- trunk/src/nl/tudelft/in4325/index/Indexer.java
+++ trunk/src/nl/tudelft/in4325/index/Indexer.java
@@ -1,54 +1,79 @@
+/**
+ * This class takes care of the indexing procedure of the documents
+ * 
+ * @author Thijs Zandvliet
+ */
+
 package nl.tudelft.in4325.index;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.Scanner;
-import java.util.Vector;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 
 import nl.tudelft.in4325.main.Main;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-
 public class Indexer
 {
-	
-	private Hashtable<String, Vector<Integer>> index;
+	/* variable declarations */ 
+	private Hashtable<String, ArrayList<Integer>> index;
 	private Hashtable<Integer, String> docs;
 
+	
+	/**
+	 * Constructor
+	 */
 	public Indexer()
 	{
-		index = new Hashtable<String, Vector<Integer>>();
+		/* create a new Hashtable for the index and the documents */
+		index = new Hashtable<String, ArrayList<Integer>>();
 		docs = new Hashtable<Integer, String>();
 	}
 	
 	
+	/**
+	 * Initialize
+	 * Read all the documents in the directory docs
+	 * 
+	 * @author Thijs Zandvliet
+	 * @throws FileNotFoundException
+	 */
 	public void init() throws FileNotFoundException
 	{
-		Vector<Integer> values;
+		/* define a new ArrayList for the found words */
+		ArrayList<Integer> values;
+		
+		/* define a new file object with the directory to read and retrieve all the content */
 		File dir = new File("src/docs");
 		String[] children = dir.list();
+		
+		/* if the directory isn't empty... */
 		if(children != null)
 		{
+			/* document counter */
 			int i = 0;
+			
+			/* for each filename in the directory... */
 			for(String filename : children)
 			{
+				/* if the filename doesn't start with a dot, then... , otherwise it could be a configuration file like .svn */
 				if(filename.charAt(0) != '.')
 				{
+					/* increase the document counter */
 					i++;
+					
+					/* create a new scanner for the found file and put the filename to the docs ArrayList */
 					Scanner sc = new Scanner(new File("src/docs/" + filename));
 					docs.put(i, filename);
 					
+					/* as long as the file contains words... */
 					while(sc.hasNext())
 					{
+						/* save the next word into the string key */
 						String key = Main.formatString(sc.next());
-						//System.out.println(filename + ": " + key);
+						
+						/* if the key already exists in the index, than add the document to the list if it isn't already there */
 						if(index.containsKey(key))
 						{
 							values = index.get(key);
@@ -56,9 +81,11 @@ public class Indexer
 								values.add(i);
 							index.put(key, values);
 						}
+						
+						/* else make a new ArrayList and add it to a new key into the Hashtable */
 						else
 						{
-							values = new Vector<Integer>();
+							values = new ArrayList<Integer>();
 							values.add(i);
 							index.put(key, values);
 						}	
@@ -69,47 +96,13 @@ public class Indexer
 	}
 	
 	
-	public void indexData()
-	{
-		try
-		{
-			File xmlfile = new File("src/reuters/reut2-000.sgm");
-			
-			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-			DocumentBuilder db = dbf.newDocumentBuilder();
-			Document doc = db.parse(xmlfile);
-			doc.getDocumentElement().normalize();
-			
-			NodeList list = doc.getElementsByTagName("REUTERS");
-			for(int i = 0; i < list.getLength(); i++)
-			{
-				Node node = list.item(i);
-				
-				if(node.getNodeType() == Node.ELEMENT_NODE)
-				{
-					Element elMain = (Element)node;
-					
-					NodeList listText = elMain.getElementsByTagName("TEXT");
-					
-					Element elTitle = (Element)listText.item(0);
-					NodeList listTitle = elTitle.getElementsByTagName("TITLE");
-					Element elTitleValue = (Element)listTitle.item(0);
-					NodeList listTitleValue = elTitleValue.getChildNodes();
-					System.out.println("title: " + ((Node) listTitleValue.item(0)).getNodeValue());
-					
-					//Element elBody = (Element)listText.item(1);
-					//NodeList listBody = elBody.getElementsByTagName("BODY");
-				}
-			}
-		}
-		catch(Exception ex)
-		{
-			System.err.println(ex);
-		}
-	}
-	
-	
-	public Hashtable<String, Vector<Integer>> getIndex()
+	/**
+	 * Return the index
+	 * 
+	 * @author Thijs Zandvliet
+	 * @return index
+	 */
+	public Hashtable<String, ArrayList<Integer>> getIndex()
 	{
 		return index;
 	}
