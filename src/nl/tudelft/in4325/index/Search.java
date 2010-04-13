0c3e7a159314670cf7f6f0174283bd9aae6e4122
ktslabbie <ktslabbie@d204f34c-dc34-e9b8-d593-7c8555d54f4f>
2010-04-13 20:24:09 +0000

    aspect 5 + bugfixes + more docs

--- src/nl/tudelft/in4325/index/Search.java
+++ src/nl/tudelft/in4325/index/Search.java
@@ -7,10 +7,14 @@
 package nl.tudelft.in4325.index;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.PriorityQueue;
 import java.util.Scanner;
+import java.util.TreeSet;
+import java.util.Vector;
 
 import nl.tudelft.in4325.main.Main;
 
@@ -19,6 +23,7 @@ public class Search {
 	/* variable declarations */
 	private static ArrayList<Hashtable<Integer,Integer>> results;
 	private static Hashtable<String,Hashtable<Integer,Integer>> pertermResults;
+	private static Hashtable<String,Hashtable<Integer,Integer>> optimizedResults;
 	
 	/**
 	 * Performs a boolean search on the given query
@@ -26,8 +31,15 @@ public class Search {
 	 * @author Thijs Zandvliet
 	 * @param query
 	 */
-	public static void booleanSearch(String query)
+	public static void booleanSearch(String query, int mode)
 	{
+		/* if in optimize cosine ranking mode */
+		if(mode == 1)
+		{
+			/* strip the opening $ character */
+			query = query.substring(1);
+		}
+		
 		/* create a new scanner which reads the query one word at the time */
 		Scanner sc = new Scanner(query);
 		
@@ -68,7 +80,7 @@ public class Search {
 					results.add(result);
 					pertermResults.put(keyword, result);
 				}
-				
+
 				/* receive the first two ArrayLists */
 				Hashtable<Integer,Integer> r1 = results.get(0);
 				Hashtable<Integer,Integer> r2 = results.get(1);
@@ -99,15 +111,108 @@ public class Search {
 		}
 		
 		/* if there are results to be processed */
-		if(!pertermResults.isEmpty())
+		if(!pertermResults.isEmpty() && mode == 0)
 		{
 			// rank them
 			PriorityQueue<RankedNode> ranked = Ranking.getHeap(pertermResults);
+			//System.out.println(pertermResults);
+			printRanked(ranked);
+			printResult();
+		}
+		
+		/* if there are results to be processed and optimization to be done */
+		else if(!pertermResults.isEmpty() && mode == 1)
+		{
+			System.out.println("Running optimized cosine ranking.");
+			
+			/* we only want to process docs that contain at least half of the terms, ceilinged */
+			double size = (double)pertermResults.size()/2;
+			size = Math.ceil(size);
+			
+			System.out.println("Unoptimized result: " + pertermResults);
+			
+			/* filter the results */
+			optimizeResults((int)size);
+			
+			System.out.println("Optimized result: " + optimizedResults);
+			
+			// rank only the docs that contain 'size' of the terms
+			PriorityQueue<RankedNode> ranked = Ranking.getHeap(optimizedResults);
 			
 			printRanked(ranked);
-			//printResult();
+			printResult();
 		}
 	}
+	
+	/**
+	 * Selects only the documents that contain the amount of terms determined by parameter size,
+	 * and adds them to variable optimizedResults.
+	 * 
+	 * @author Kristian Slabbekoorn
+	 * @param size
+	 */
+	@SuppressWarnings("unchecked")
+	public static void optimizeResults(int size) {
+		
+		/* copy the full result list to the optimized list */
+		optimizedResults = (Hashtable<String,Hashtable<Integer,Integer>>)pertermResults.clone();
+		
+		/* retrieve all query terms */
+		Vector<String> terms = new Vector<String>(pertermResults.keySet());
+		
+		/* retrieve the value collection belonging to each term (<docs containing this word, number of instances>)*/
+		Collection<Hashtable<Integer, Integer>> values = pertermResults.values();
+		
+		/* construct and init iterator for values */
+		Iterator<Hashtable<Integer, Integer>> ti = values.iterator();
+	    Hashtable<Integer, Integer> currentTable = ti.next();
+	    
+	    /* retrieve a sorted union set of all documents that contain at least one of the terms */
+	    TreeSet<Integer> allDocuments = new TreeSet<Integer>();
+	    allDocuments.addAll(currentTable.keySet());
+	    while(ti.hasNext()){
+	    	currentTable = ti.next();
+	    	allDocuments.addAll(currentTable.keySet());
+	    }
+	    
+		/* construct and init iterator for all documents */
+	    Iterator<Integer> di = allDocuments.iterator();
+	    int currentDoc;
+	    
+	    /* create counter that will count the number of query terms present in the current doc */
+	    int counter;
+	    
+	    /* while there are still docs in the union set... */
+	    while(di.hasNext()){
+	    	
+	    	/* get next doc */
+	    	currentDoc = di.next();
+	    	
+	    	/* reinit values iterator */
+		    ti = values.iterator();
+		    
+		    /* init counter */
+		    counter = 0;
+		    
+		    /* iterate over hashtables linked to each term */
+		    while(ti.hasNext()){
+		    	currentTable = ti.next();
+		    	/* if the current hashtable contains the current document, increase the counter */
+				if(currentTable.containsKey(currentDoc))
+					counter++;
+			}
+		    
+		    /* if the current document contained less than 'size' terms... */
+		    if(counter < size) {
+		    	
+		    	/* remove the current document from the result list */
+		    	for (int i = 0; i < terms.size(); i++) {
+		 			optimizedResults.get(terms.get(i)).remove(currentDoc);
+		 		}
+		    }
+	    }
+	}
+	
 	public static void printRanked(PriorityQueue<RankedNode> p){
 		String output = "Ranked result (top 3 of "+p.size()+")";
 		RankedNode n;
@@ -167,21 +272,27 @@ public class Search {
 	 */
 	public static void booleanOR(Hashtable<Integer,Integer> r1, Hashtable<Integer,Integer> r2)
 	{
-		/* detect empty ArrayLists */
+		/* detect empty HashTables */
 		if(detectNull(r1, r2)) return;
 		
-		/* add a null-value at the end of both ArrayLists for the iterator */
-		r1.put(null,null);
-		r2.put(null,null);
+		/* sort hashtable elements */
+		Vector<Integer> v1 = new Vector<Integer>(r1.keySet());
+		Vector<Integer> v2 = new Vector<Integer>(r2.keySet());
+	    Collections.sort(v1);
+	    Collections.sort(v2);
+	    
+	    /* add a null-value at the end of both Vectors for the iterator */
+	    v1.add(null);
+	    v2.add(null);
 		
 		/* create two iterators for both ArrayLists */
-		Iterator<Integer> p1 = r1.keySet().iterator();
-		Iterator<Integer> p2 = r2.keySet().iterator();
+		Iterator<Integer> p1 = v1.iterator();
+		Iterator<Integer> p2 = v2.iterator();
 		
 		/* set the two first Integers so they can be compared */
 		Integer i1 = p1.next();
 		Integer i2 = p2.next();
-		
+
 		/* create a new ArrayList for the answer */
 		Hashtable<Integer,Integer> answer = new Hashtable<Integer,Integer>();
 		
@@ -234,16 +345,22 @@ public class Search {
 			return;
 		}
 		
-		/* add a null-value at the end of both ArrayLists for the iterator */
-		r1.put(null,null);
-		r2.put(null,null);
-		
-		/* create a new ArrayList for the answer */
+		/* create a new HashTable for the answer */
 		Hashtable<Integer,Integer> answer = new Hashtable<Integer,Integer>();
 		
+		/* sort hashtable elements */
+		Vector<Integer> v1 = new Vector<Integer>(r1.keySet());
+		Vector<Integer> v2 = new Vector<Integer>(r2.keySet());
+	    Collections.sort(v1);
+	    Collections.sort(v2);
+	    
+	    /* add a null-value at the end of both Vectors for the iterator */
+	    v1.add(null);
+	    v2.add(null);
+		
 		/* create two iterators for both ArrayLists */
-		Iterator<Integer> p1 = r1.keySet().iterator();
-		Iterator<Integer> p2 = r2.keySet().iterator();
+		Iterator<Integer> p1 = v1.iterator();
+		Iterator<Integer> p2 = v2.iterator();
 		
 		/* set the two first Integers so they can be compared */
 		Integer i1 = p1.next();
@@ -288,16 +405,22 @@ public class Search {
 		/* detect empty ArrayLists */
 		if(detectNull(r1, r2)) return;
 		
-		/* add a null-value at the end of both ArrayLists for the iterator */
-		r1.put(null,null);
-		r2.put(null,null);
-		
 		/* create a new ArrayList for the answer */
 		Hashtable<Integer,Integer> answer = new Hashtable<Integer,Integer>();
 		
+		/* sort hashtable elements */
+		Vector<Integer> v1 = new Vector<Integer>(r1.keySet());
+		Vector<Integer> v2 = new Vector<Integer>(r2.keySet());
+	    Collections.sort(v1);
+	    Collections.sort(v2);
+	    
+	    /* add a null-value at the end of both Vectors for the iterator */
+	    v1.add(null);
+	    v2.add(null);
+		
 		/* create two iterators for both ArrayLists */
-		Iterator<Integer> p1 = r1.keySet().iterator();
-		Iterator<Integer> p2 = r2.keySet().iterator();
+		Iterator<Integer> p1 = v1.iterator();
+		Iterator<Integer> p2 = v2.iterator();
 		
 		/* set the two first Integers so they can be compared */
 		Integer i1 = p1.next();
